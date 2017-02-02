640e923833200ac0ebdee80669e4f5fb4dc0eea4
niwacha <kenzonico@gmail.com>
2017-02-02 12:06:14 -0800

    hash key… marche.

--- components/JustMap.js
+++ components/JustMap.js
@@ -32,6 +32,15 @@ const Menu = require('./Menu');
 import {FBLogin, FBLoginManager} from 'react-native-facebook-login';
 import FBLoginView from './FBLoginView';
 
+FBLoginManager.setLoginBehavior(FBLoginManager.LoginBehaviors.Web); // defaults to Native
+
+// FBLoginManager.loginWithPermissions(["email","user_friends"], function(error, data){
+//   if (!error) {
+//     console.log("Login data: ", data);
+//   } else {
+//     console.log("Error: ", error);
+//   }
+// })
 
 export default class JustMap extends React.Component {
 
@@ -193,7 +202,6 @@ export default class JustMap extends React.Component {
     }
 
     onPressMarker(e) {
-
       this.state.showViewDetails = true;
       this.setState({
         region: {
@@ -260,6 +268,7 @@ export default class JustMap extends React.Component {
 
     }
 
+
   render() {
     return (
       <View style={styles.container}>
@@ -277,7 +286,6 @@ export default class JustMap extends React.Component {
 
 
               {this.state.locations.map((location,i) =>{
-
                 return (
                   <MapView.Marker
                     key={location.key}
@@ -313,18 +321,36 @@ export default class JustMap extends React.Component {
             </MapView>
 
 
-            <FBLogin
-                buttonView={<FBLoginView />}
-                ref={(fbLogin) => { this.fbLogin = fbLogin }}
-                loginBehavior={FBLoginManager.LoginBehaviors.Native}
-                permissions={["email","user_friends"]}
-                onLogin={function(e){console.log(e)}}
-                onLoginFound={function(e){console.log(e)}}
-                onLoginNotFound={function(e){console.log(e)}}
-                onLogout={function(e){console.log(e)}}
-                onCancel={function(e){console.log(e)}}
-                onPermissionsMissing={function(e){console.log(e)}}
-              />
+            <FBLogin style={{ marginBottom: 10, }}
+                  ref={(fbLogin) => { this.fbLogin = fbLogin }}
+                  permissions={["email","user_friends"]}
+                  loginBehavior={FBLoginManager.LoginBehaviors.Native}
+                  onLogin={function(data){
+                    console.log("Logged in!");
+                    console.log(data);
+                  }}
+                  onLogout={function(){
+                    console.log("Logged out.");
+                  }}
+                  onLoginFound={function(data){
+                    console.log("Existing login found.");
+                    console.log(data);
+                  }}
+                  onLoginNotFound={function(){
+                    console.log("No user logged in.");
+                  }}
+                  onError={function(data){
+                    console.log("ERROR");
+                    console.log(data);
+                  }}
+                  onCancel={function(){
+                    console.log("User cancelled.");
+                  }}
+                  onPermissionsMissing={function(data){
+                    console.log("Check permissions!");
+                    console.log(data);
+                  }}
+                />
 
 
 
@@ -358,6 +384,48 @@ export default class JustMap extends React.Component {
   }
 }
 
+
+class Login extends Component {
+  render() {
+    var _this = this;
+    return (
+      <FBLogin style={{ marginBottom: 10, }}
+        ref={(fbLogin) => { this.fbLogin = fbLogin }}
+        permissions={["email","user_friends"]}
+        loginBehavior={FBLoginManager.LoginBehaviors.Native}
+        onLogin={function(data){
+          console.log("Logged in!");
+          console.log(data);
+          _this.setState({ user : data.credentials });
+        }}
+        onLogout={function(){
+          console.log("Logged out.");
+          _this.setState({ user : null });
+        }}
+        onLoginFound={function(data){
+          console.log("Existing login found.");
+          console.log(data);
+          _this.setState({ user : data.credentials });
+        }}
+        onLoginNotFound={function(){
+          console.log("No user logged in.");
+          _this.setState({ user : null });
+        }}
+        onError={function(data){
+          console.log("ERROR");
+          console.log(data);
+        }}
+        onCancel={function(){
+          console.log("User cancelled.");
+        }}
+        onPermissionsMissing={function(data){
+          console.log("Check permissions!");
+          console.log(data);
+        }}
+      />
+    );
+  }
+};
 
 JustMap.propTypes = {
     provider: MapView.ProviderPropType,
