85acee8bb8259b0be4fe557dd5a7891b1452cdd6
alan345 <alan345@gmail.com>
2017-03-03 17:57:25 -0800

    Revert "Redesign of the trip list, in trip idea screen"

--- components/Menu.js
+++ components/Menu.js
@@ -12,7 +12,6 @@ const {
   View,
   Image,
   Text,
-  TouchableWithoutFeedback
 } = require('react-native');
 const { Component } = React;
 
@@ -48,33 +47,30 @@ module.exports = class Menu extends Component {
   render() {
     return (
       <ScrollView scrollsToTop={false} style={styles.menu}>
+        <View style={styles.avatarContainer}>
+          <Image
+            style={styles.avatar}
+            source={{uri: this.state.userData.picture.data.url}}
+          />
+          <Text style={styles.name}>{this.state.userData.name}</Text>
+        </View>
 
 
-          <TouchableWithoutFeedback  onPress={() => this.props.onItemSelected('Contacts')}>
-                <View style={styles.avatarContainer}>
-                      <Image
-                        style={styles.avatar}
-                        source={{uri: this.state.userData.picture.data.url}}
-                      />
-                      <Text style={styles.name}>{this.state.userData.name}</Text>
-                </View>
-            </TouchableWithoutFeedback>
 
+        <Text
+          onPress={() => this.props.onItemSelected('MyMaps')}
+          style={styles.item}>
+          My Maps
+        </Text>
 
-            <Text
-              onPress={() => this.props.onItemSelected('MyMaps')}
-              style={styles.item}>
-              My Trips
-            </Text>
+        <Text
+          onPress={() => this.props.onItemSelected('Contacts')}
+          style={styles.item}>
+          Profile
+        </Text>
 
-            <Text
-              onPress={() => this.props.onItemSelected('MyMaps')}
-              style={styles.item}>
-              Trip Ideas
-            </Text>
-
-            <FBLoginView
-              updateUserData={this.onChangeUserData.bind(this)}/>
+        <FBLoginView
+          updateUserData={this.onChangeUserData.bind(this)}/>
 
 
       </ScrollView>
